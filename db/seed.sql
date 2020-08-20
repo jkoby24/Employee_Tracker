6fac37f6a24bf1b28b5d3f046bb5cb0884a80dd4
jkoby24 <63992346+jkoby24@users.noreply.github.com>
2020-08-19 20:28:20 -0400

    final commit. working app

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,16 @@
+DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db; 
 
 USE employee_db; 
 
 CREATE TABLE department( 
-id INT AUTO_INCREMENT PRIMARY KEY, 
-name VARCHAR(30) NOT NULL 
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+name VARCHAR (30) NOT NULL
 ); 
 
 CREATE TABLE role( 
-id INT AUTO_INCREMENT PRIMARY KEY, 
+ id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
  title VARCHAR(30) NOT NULL, 
  salary DECIMAL (8,2) NOT NULL, 
  department_id INT NOT NULL, 
@@ -25,11 +27,12 @@ id INT AUTO_INCREMENT PRIMARY KEY,
  FOREIGN KEY(manager_id) REFERENCES employee(id)
  ); 
 
+SELECT * from department;
 
 
 
-INSERT INTO departments
-    (department_name)
+INSERT INTO department
+    (name)
 VALUES
     ('Sales'),
     ('Accounting'),
@@ -37,44 +40,59 @@ VALUES
     ('Marketing'),
     ('Executive');
 
-INSERT INTO roles
+INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Marketing_Rep', 90000, 20),
-    ('Marketing_Manager', 130000, 20),
-    ('Data_analyst', 150000, 60),
-    ('IT_Programmer', 130000, 60),
-     ('Sales_Rep', 80000, 80),
-    ('Sales_Manager', 120000, 80),
-    ('VP', 300000, 90),
-    ('Account_Manager', 120000, 110),
-    ('Accountant', 100000, 110);
+	('Sales_Manager', 120000, 1),
+	('Sales_Rep', 80000, 1),
+	('Account_Manager', 120000, 2),
+    ('Accountant', 100000, 2),
+    ('Project_Manager', 180000,3),
+    ('Data_analyst', 150000, 3),
+    ('IT_Programmer', 130000, 3),
+    ('Marketing_Manager', 130000, 4),
+    ('Marketing_Rep', 90000, 4),
+    ('VP', 300000, 5);
+    
+     
+   SELECT * FROM role; 
+    
     
 
-INSERT INTO employees
+INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('Steven', 'King', 1, NULL),
-    ('Neena', 'Kochar', 2, 5 ),
-    ('Nia', 'Hall', 3, NULL),
-    ('Bruce', 'Austin', 4, ),
-    ('Alex', 'Ernst', 5, NULL ),
-    ('Peter', 'Tucker', 6, 5),
-    ('Chris', 'Olsen', 7, NULL),
-    ('Karen', 'Vargas', 8, 1),
-    ('Jennifer', 'Whalen', 9, 7),
-    ('Pat', 'Fay', 11, 5),
-    ('Douglass', 'Grant', 12, 3),
-    ('Julia', 'Bull', 13, 1),
-    ('Alyssa', 'Hutton', 14, 3),
-    ('Johnathan', 'Taylor', 15, 3),
-    ('Chris', 'Olave', 16, 3),
-    ('Lisa', 'Ozer', 17, 7),
-    ('Patrick', 'Sully', 18, 5),
-    ('Kobe', 'Bryant', 19, 5),
-    ('LeBron', 'James', 20, 5),
-    ('Rick', 'James', 21, 5),
-    ('Rick', 'Fox', 22, 7),
-    ('Ahmed', 'Jacobs', 23, 7),
-    ('Kelly', 'Chung', 24, 7),
-    ('Halle', 'Berry', 25, 7);
+    ('Neena', 'Kochar', 2, 1 ),
+    ('Nia', 'Hall', 2, 1),
+    ('Bruce', 'Austin', 2, 1),
+    ('Alex', 'Ernst', 3, NULL ),
+    ('Peter', 'Tucker', 4, 3),
+    ('Chris', 'Olsen', 4, 3),
+    ('Karen', 'Vargas', 5, NULL),
+    ('Jennifer', 'Whalen', 6, 5),
+    ('Pat', 'Fay', 6, 5),
+    ('Douglass', 'Grant', 7, 5),
+    ('Julia', 'Bull', 7, 5),
+    ('Alyssa', 'Hutton', 7, 5),
+    ('Johnathan', 'Taylor', 7, 5),
+    ('Chris', 'Olave', 7, 5),
+    ('Lisa', 'Ozer', 7, 5),
+    ('Patrick', 'Sully', 8, NULL),
+    ('Kobe', 'Bryant', 9, 8),
+    ('LeBron', 'James', 9, 8),
+    ('Rick', 'James', 9, 8),
+    ('Rick', 'Fox', 9, 8),
+    ('Ahmed', 'Jacobs', 9, 8),
+    ('Kelly', 'Chung', 9, 8),
+    ('Halle', 'Berry', 10, NULL);
+
+SELECT employee.id, employee.first_name AS "First Name", employee.last_name AS "Last Name", role.title, department.name AS "Department", role.salary AS "Salary", CONCAT(manager.first_name," ",manager.last_name) AS "Manager"
+
+FROM employee 
+LEFT JOIN role 
+ON role.id = employee.role_id 
+LEFT JOIN department  
+ON department.id = role.department_id
+LEFT JOIN employee manager ON manager.id = employee.manager_id
+ORDER BY employee.id;
---
+++ index.js
@@ -0,0 +1,277 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const Table = require('console.table');
+const { type } = require('os');
+
+connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Sancho$07',
+  database: 'employee_db',
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'Add department',
+        'Add role',
+        'Add employee',
+        'View all employees',
+        'View employees by department',
+        'View all departments',
+        'View all roles',
+        'Update employee role',
+        'Exit',
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case 'Add department':
+          addDepartment();
+          break;
+
+        case 'Add role':
+          addRole();
+          break;
+
+        case 'Add employee':
+          addEmployee();
+          break;
+
+        case 'View all employees':
+          viewEmployees();
+          break;
+
+        case 'View employees by department':
+          viewByDept();
+          break;
+
+        case 'View all departments':
+          viewAllDept();
+          break;
+
+        case 'View all roles':
+          viewAllRoles();
+          break;
+
+        case 'Update employee role':
+          updateRoles();
+          break;
+
+        case 'Exit':
+          connection.end();
+          break;
+      }
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: 'department',
+        type: 'input',
+        message: 'Enter new departments name',
+      },
+    ])
+    .then(function (answer) {
+      query = `INSERT INTO department SET ?`;
+      connection.query(
+        query,
+        {
+          name: answer.department,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('Added new department!');
+          runSearch();
+        }
+      );
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: 'title',
+        type: 'input',
+        message: 'Enter title of new role',
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: 'Enter salary for role',
+      },
+      {
+        name: 'departmentid',
+        type: 'list',
+        message: 'Select department id for role',
+        choices: ['1', '2', '3', '4', '5'],
+      },
+    ])
+    .then(function (answer) {
+      query = `INSERT INTO role SET ?`;
+      connection.query(
+        query,
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentid,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('Added new role!');
+          runSearch();
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: 'firstname',
+        type: 'input',
+        message: 'Enter employees first name',
+      },
+      {
+        name: 'lastname',
+        type: 'input',
+        message: 'Enter employees last name',
+      },
+      {
+        name: 'roleid',
+        type: 'list',
+        message: 'Select role id',
+        choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      },
+      {
+        name: 'managerid',
+        type: 'list',
+        message: 'Select manager id',
+        choices: ['1', '3', '5', '8', '10'],
+      },
+    ])
+    .then(function (answer) {
+      query = `INSERT INTO employee SET ?`;
+      connection.query(
+        query,
+        {
+          first_name: answer.firstname,
+          last_name: answer.lastname,
+          role_id: answer.roleid,
+          manager_id: answer.managerid,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log('Added new employee!');
+          runSearch();
+        }
+      );
+    });
+}
+
+function viewEmployees() {
+  query = `SELECT employee.id, employee.first_name AS "First Name", employee.last_name AS "Last Name", role.title, department.name AS "Department", role.salary AS "Salary",
+  FROM employee 
+  LEFT JOIN role 
+  ON role.id = employee.role_id 
+  LEFT JOIN department  
+  ON department.id = role.department_id
+  ORDER BY employee.id;`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+}
+
+function viewByDept() {
+  connection.query('SELECT * FROM department', function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: 'department',
+        type: 'list',
+        message: 'Select department to view',
+        choices: ['Sales', 'Accounting', 'IT', 'Marketing', 'Executive'],
+      })
+      .then(function (answer) {
+        query = `SELECT employee.id, employee.first_name AS "First Name", employee.last_name AS "Last Name", role.title, department.name AS "Department", role.salary AS "Salary"
+        FROM employee 
+        LEFT JOIN role 
+        ON role.id = employee.role_id 
+        LEFT JOIN department  
+        ON role.department_id = department.id WHERE department.name = '${answer.department}'`;
+        connection.query(query, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          runSearch();
+        });
+      });
+  });
+}
+
+function viewAllDept() {
+  query = `SELECT id, name AS "Departments" FROM department`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+}
+
+function viewAllRoles() {
+  query = `SELECT id, title AS "Title" FROM role`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+}
+
+function updateRoles() {
+  inquirer
+    .prompt([
+      {
+        name: 'id',
+        type: 'input',
+        message: 'Enter id of employee',
+      },
+      {
+        name: 'newrole',
+        type: 'input',
+        message: 'Enter employees new role id',
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        `UPDATE employee SET ? WHERE ?`,
+        [
+          {
+            role_id: answer.newrole,
+          },
+          {
+            id: answer.id,
+          },
+        ],
+        (err) => {
+          if (err) throw err;
+          console.log('Updated employee!');
+          runSearch();
+        }
+      );
+    });
+}
